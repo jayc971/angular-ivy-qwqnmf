7f6be02efc798b8357885ef76405dd469f726ab8
jayc971 <jayanika1997@gmail.com>
2021-12-23 00:27:05 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, VERSION } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  myControl = new FormControl();
   name = 'Angular ' + VERSION.major;
-  options = ['dsadsa','dsadsa'];
+  options = ['dsadsa', 'dsadsa', '123'];
 }
